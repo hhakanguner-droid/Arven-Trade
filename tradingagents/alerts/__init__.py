9079d75e2473bc14df972934f82ca0de2af7ea88
hhakanguner-droid <hhakanguner@gmail.com>
2026-08-24 17:04:08 +0300

    fix: install Phase 10 alert hardening

--- tradingagents/alerts/__init__.py
+++ tradingagents/alerts/__init__.py
@@ -1,13 +1,16 @@
 """ARVEN Trade watchlist and notification primitives."""
 
+from . import service as _service
 from .models import AlertSourceStatus, WatchlistAlert, WatchlistAlertBatch
-from .service import (
-    AlertStateStore,
-    KapWatchlistAlertService,
-    WatchlistStore,
-    classify_kap_disclosure,
-    create_watchlist_alert_service,
-)
+from .phase10_hardening import install as _install_phase10_hardening
+
+_install_phase10_hardening(_service)
+
+AlertStateStore = _service.AlertStateStore
+KapWatchlistAlertService = _service.KapWatchlistAlertService
+WatchlistStore = _service.WatchlistStore
+classify_kap_disclosure = _service.classify_kap_disclosure
+create_watchlist_alert_service = _service.create_watchlist_alert_service
 
 __all__ = [
     "AlertSourceStatus",
