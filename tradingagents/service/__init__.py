d0e496fbded2e35e070cb8960a391e2043db47ae
hhakanguner-droid <hhakanguner@gmail.com>
2026-08-26 15:27:19 +0300

    feat: export phase13 history availability state

--- tradingagents/service/__init__.py
+++ tradingagents/service/__init__.py
@@ -4,12 +4,13 @@ FastAPI is kept behind the optional ``web`` extra; importing this package does
 not require the web stack.
 """
 
-from .core import AnalysisService
+from .core import AnalysisService, HistoryUnavailable
 from .jobs import AnalysisJobStore, IdempotencyConflict, QueueCapacityExceeded
 
 __all__ = [
     "AnalysisJobStore",
     "AnalysisService",
+    "HistoryUnavailable",
     "IdempotencyConflict",
     "QueueCapacityExceeded",
 ]
